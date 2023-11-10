109166e98011db00905512833edb11354b9557e2
Arminas Attas <106035711+ZemHyre@users.noreply.github.com>
2023-11-10 23:58:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PortfolioWebsite/',
   build: {
     outDir: 'build',
-    base: '/PortfolioWebsite/'
   },
   plugins: [react()],
 })
+
